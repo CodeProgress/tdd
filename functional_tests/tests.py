28bad23ec1e75e87441b5828db78f1c8bbadddea
CodeProgress <codeprogress1@gmail.com>
2015-08-26 23:47:17 -0400

    Refactor

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -17,10 +17,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retreive_it_later(self):
-        
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # She notices tht hte page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -50,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
         
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacockfeathers to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -60,9 +59,6 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-        
-if __name__ == '__main__':
-    unittest.main()
 
     
 
